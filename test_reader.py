70d4e5069fecee58b8172d5b27bd05dcbe481055
Ankit Vani <a@nevitus.org>
2019-03-26 11:49:59 -0400

    Added reader test

---
+++ test_reader.py
@@ -0,0 +1,12 @@
+from pylego.misc import save_comparison_grid
+
+from readers.moving_mnist import MovingMNISTReader
+
+
+if __name__ == '__main__':
+    reader = MovingMNISTReader()
+    for i, batch in enumerate(reader.iter_batches('train', 4, max_batches=5)):
+        print(batch.size())
+        if i < 3:
+            batch = batch[0].numpy().reshape(20, 1, 28, 28)
+            save_comparison_grid('seq%d.png' % i, batch)
